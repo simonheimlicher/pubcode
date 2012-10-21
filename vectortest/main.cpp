1391502c3ade480b3268f8ff2dd5adf0fb621a5d
Simon Heimlicher <sh@srvr.ch>
2012-10-21 12:21:57 -0400

    Initial Commit

---
+++ vectortest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vectortest
+//
+//  Created by Simon Heimlicher on 21.10.12.
+//  Copyright (c) 2012 Simon Heimlicher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
